a03df2c98596233643041a1a7c66789a3d2c5f0a
Rotem Abir <rotem.abir@gmail.com>
2021-05-03 11:44:25 +0200

    feat: force open PDF on browser, android
    link to PDF will be forced to open on browser (via google docs) instead of downloaidng
    iOS doesnt change

--- brochures.js
+++ brochures.js
@@ -6,9 +6,10 @@ $w.onReady(function () {
     let propertyUrl = $w("#dynamicDataset").getCurrentItem().url + '?lang=he';
     let brochureUrl = $w("#dynamicDataset").getCurrentItem().qr;
 
-	setTimeout(() => { $w('#description').text = propertyName; }, 500);
+	setTimeout(() => { $w('#description').text = propertyName; }, 300);
 
 	if(brochureUrl) {
+		brochureUrl = "http://docs.google.com/gview?embedded=true&url=" + brochureUrl
     	wixLocation.to(brochureUrl);
 		setTimeout(() => { wixLocation.to(propertyUrl);	}, 2000); // if brochure doensnt load
 	}
